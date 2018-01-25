81f9133ad4c5c218e5fec33f942de772d36e15d6
Daniel Keen <keendaniel01@gmail.com>
2018-01-25 11:42:58 +0000

    Refactor specs to be clearer. Add comments to increase clarity of docking_station.rb

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,21 +1,20 @@
 require_relative "bike"
 
 class DockingStation
-  #this replaces bike method below
+  #this replaces bike method below - allows .bike to read the @bike variable
   attr_reader :bike
-
+  #method is initialized as an empty docking station with a collection to hold Bike objects.
   def initialize
-
     @bike_rack = []
-
   end
-
+  #if there are bikes in the rack, one is taken from the end, else an error is given
   def release_bike
     # Create a variable = Instantiation of class to be returned
     raise "No bikes available" unless @bike_rack.length > 0
     @bike_rack.pop
   end
-
+  #a bike can be docked - pushed into the bike_rack. if the rack has 20 bikes, it is full
+  #and an error is raised
   def dock(bike)
     raise "Dock is full" unless @bike_rack.length < 20
     @bike_rack.push(@bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,39 +4,38 @@ describe DockingStation do
   # Expecting that DockingStation class responds to release_bike method
   # Create a new variable bike = subject(instatiation of DockingStation class).release_bike
   # Expecting object bike to works, when calling method working? should returns true
+  it { is_expected.to respond_to :bike }
+  
   describe '#release_bike' do
     it { is_expected.to respond_to :release_bike }
 
-    it "releases a working bike" do
-      bike = Bike.new
-      expect(bike).to be_working
-    end
+    # it "Bike should be a working bike" do
+    #   bike = Bike.new
+    #   expect(bike).to be_working
+    # end
 
-    it 'releases a bike' do
+    it "releases a bike from the bike bike_rack" do
       10.times{subject.dock Bike.new}
       expect(subject.release_bike).to be_an_instance_of(Bike)
     end
 
-    it "raises error when not bike is in the docking station" do
+    it "raises error when no bike is in the docking station" do
       expect{subject.release_bike}.to raise_error("No bikes available")
     end
   end
 
   describe '#dock' do
-
     it { is_expected.to respond_to(:dock).with(1).argument}
 
-    it "can store a bike " do
+    it "can store a bike" do
       bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq bike
     end
 
     it "raises an error when trying to dock a bike when twenty bikes are already docked" do
-      expect{21.times{ subject.dock(Bike.new)}}.to raise_error("Dock is full")
+      expect{ 21.times{ subject.dock(Bike.new) } }.to raise_error("Dock is full")
     end
   end
-
-    it { is_expected.to respond_to :bike }
 
     it 'Should return bike if available' do
       bike = Bike.new
@@ -45,7 +44,7 @@ describe DockingStation do
     end
 
   describe '#initialize' do
-    it "should have a capacity of 20 bikes"do
+    it "should have a capacity of 20 bikes" do
     expect{ 20.times{ subject.dock Bike.new }}.to_not raise_error
     end
 
